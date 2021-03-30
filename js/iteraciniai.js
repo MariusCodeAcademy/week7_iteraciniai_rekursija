194120520edcad388ffc7b4a332a543bf22d8ee3
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-30 10:44:03 +0300

    9

--- js/iteraciniai.js
+++ js/iteraciniai.js
@@ -1,6 +1,6 @@
 "use strict";
 console.log("iteraciniai");
-
+//       index   0  1  2 ...
 const numbers = [1, 2, 3, 5.3, 8, -8, -5.5, -6, -16, -5, -9, 4.2, 5, 12, -6, 45];
 console.log(numbers);
 //  * forEach
@@ -12,10 +12,32 @@ let doubles = numbers.map((num) => num * 2);
 //  4. Sukurkite ir atvaizduokite masyvą su kvadratu pekltomis reikšmėmis;
 let power = numbers.map((num) => num ** 2);
 //  5. Sukurkite ir atvaizduokite masyvą su reikšmėmis padaugintomis iš jų vietos masyve indekso
-
+let indexTimesNum = numbers.map((num, index) => {
+  return num * index;
+});
 //  6. Atrinkti tiktai teigimų elementų masyvą
+let positive = numbers.filter((num) => num > 0);
+
+// 7. Atrinkti tiktai neigiamų elementų masyvą
+let negatives = numbers.filter((num) => !(num > 0));
+// 8. Atrinkti tiktai lyginių skaičių masyvą
+let evenNums = numbers.filter((num) => num % 2 === 0);
+
+// 9. Atrinkti tiktai nelyginių skaičių masyvą
+let oddNums = numbers.filter((num) => num % 2 !== 0);
+
+// 10. Sukurkite masyvą kuriame visos neigiamos reikšmės būtų paverstos teigiamomis
+
+// 11. Atrinkti tik natūralių skaičių masyvą
+
+// 12. Suapvalinti visas masyvo reikšmes iki sveikų skaičių
 
 //  ========================= Rezultatai ==============================
 
 printMe("doubles", doubles);
 printMe("power", power);
+printMe("indexTimesNum", indexTimesNum);
+printMe("positive", positive);
+printMe("negatives", negatives);
+printMe("evenNums", evenNums);
+printMe("oddNums", oddNums);
